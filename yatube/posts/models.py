1999f5c1eb1788a8a389a747c509e2f34fe40cf7
Александр Фокин <wegnagun@bk.ru>
2022-05-13 19:41:23 +0300

    zhopka

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,7 +1,6 @@
+from core.models import CreatedModel
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.db.models import Q, F
-from core.models import CreatedModel
 
 User = get_user_model()
 
