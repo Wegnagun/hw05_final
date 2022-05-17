294bc158dfdf6534f0993062a80cc526cdaf5e0b
Александр Фокин <wegnagun@bk.ru>
2022-05-17 13:44:10 +0300

    add useful

--- yatube/useful/forms.py
+++ yatube/useful/forms.py
@@ -2,12 +2,12 @@ from django import forms
 
 
 class PercentForm(forms.Form):
-    first_num = forms.CharField(max_length=100)
-    second_num = forms.CharField(max_length=100)
+    first_num = forms.FloatField()
+    second_num = forms.FloatField()
 
-    # class Meta:
-    #     fields = ('first_num', 'second_num')
-    #     label = {'first_num': 'большее',
-    #              'second_num': 'меньшее'}
-    #     help_text = {'first_num': 'Процент от этого числа',
-    #                  'second_num': 'Процент этого числа'}
+    class Meta:
+        fields = ('first_num', 'second_num')
+        label = {'first_num': 'большее',
+                 'second_num': 'меньшее'}
+        help_text = {'first_num': 'Процент от этого числа',
+                     'second_num': 'Процент этого числа'}
